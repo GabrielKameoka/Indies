b624ae744577dcc4c8b188ba9585b9fe9d96b4ef
GabrielKameoka <gabrielkameoka@gmail.com>
2025-03-07 11:35:42 -0300

    Adicionando model musicas

---
+++ Indies/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Indies.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Indies.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    {
+        
+    }
+    
+    public DbSet<MusicasModel> Musicas { get; set; }
+}
---
+++ Indies/Models/MusicasModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Indies.Models;
+
+public class MusicasModel
+{
+    public int Id { get; set; }
+    public string Musicas { get; set; }
+    public string Artista { get; set; }
+    public DateOnly Lancamento { get; set; }
+}
